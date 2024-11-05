4c5289f3f20ba39e9bc9eee6226e7dcfd8f23a57
harunseki <harunseki@gmail.com>
2024-11-05 10:52:03 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get ; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
+
+
+/*
+ Field-Property-Variable
+ */
+
+/*
+ int x; -> Field (bir değişken direkt olarak classın içinde tanımlanması)
+ public int y {get; set;} -> Property (değişkenin bir classın içinde get ve set ile tanımlanması)
+ void test { 
+    int z; -> Variable(değişken) (metodun içinde tanımlanırsa )
+ }
+ */
