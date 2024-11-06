8e1a0e27720872d33dab8687ecc6655a58c6f811
harunseki <harunseki@gmail.com>
2024-11-06 16:44:14 +0300

    db baglantısı

--- CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -11,15 +11,14 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
         public int CategoryId { get; set; }
         public string CategoryName { get ; set; }
         public bool CategoryStatus { get; set; }
+        public List<Product> Products { get ; set; }
     }
 }
 
 
 /*
  Field-Property-Variable
- */
 
-/*
  int x; -> Field (bir değişken direkt olarak classın içinde tanımlanması)
  public int y {get; set;} -> Property (değişkenin bir classın içinde get ve set ile tanımlanması)
  void test { 
